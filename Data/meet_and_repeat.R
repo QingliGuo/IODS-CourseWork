a0a150e5799f68e5f06f97a2c51250eb1b6d16f8
QingliGuo <qingli.guo@helsinki.fi>
2018-12-07 11:47:23 +0200

    week6-v1

---
+++ Data/meet_and_repeat.R
@@ -0,0 +1,7 @@
+## By Qingli Guo
+## Dec 6 2018
+## to preprocess the wide-type of data 'BPRS' & 'RATS' to long-type
+
+## BPRS: 
+BPRS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/BPRS.txt", sep  =" ", header = T)
+head(BPRS)
